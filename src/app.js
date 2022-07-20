ec07289c478c82cdf430e0459f70052016fb527e
Mila Yoselyn Pacompia Machaca <mila.pacompia@globant.com>
2022-07-20 16:30:23 -0500

    config app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // define paths for express config
 const publicDir = path.join(__dirname, '../public')
@@ -118,7 +119,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 })
 
